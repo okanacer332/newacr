0af74baa87d160065e4ad88c6a1cbf75bd928875
okanacer332 <acer.okanumut@hotmail.com>
2025-11-27 23:48:01 +0000

    update

--- app/[locale]/(site)/page.tsx
+++ app/[locale]/(site)/page.tsx
@@ -14,10 +14,10 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  title: "AcrTech",
 
   // other metadata
-  description: "This is Home for Solid Pro"
+  description: "ACRTECH olarak işletmenizin dijital ikizini oluşturuyoruz. Sahadaki veriyi finansal zekaya dönüştüren uçtan uca ekosistem."
 };
 
 export default function Home() {
